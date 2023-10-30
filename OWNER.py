3ee5d70fb3c63e1cf52f444059203bc38b263cad
mvhamed <145144709+mvhamed@users.noreply.github.com>
2023-10-30 21:24:39 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Mvhmed"]
+OWNER_NAME = "𝙈𝙪𝙝𝙖𝙢𝙢𝙚𝙙 𝙆𝙝𝙖𝙡𝙞𝙙"
+BOT_TOKEN = "6553005927:AAH2CTrIlBh4x9xw_9InmhX9EkD0W38ti64"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/Mvhmed"
+GROUP = "https://t.me/Mvhmed"
+VIDEO = "https://telegra.ph/file/7065a4c7cbedef8befdb9.jpg"
+LOGS = "zombie2"
+
