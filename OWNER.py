f0b3a11e65d63f8b50e85494bc5cc6b1a2926e6b
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-06 19:13:05 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["Mvhmed"]
 OWNER_NAME = "𝙈𝙪𝙝𝙖𝙢𝙢𝙚𝙙 𝙆𝙝𝙖𝙡𝙞𝙙"
-BOT_TOKEN = "6553005927:AAH2CTrIlBh4x9xw_9InmhX9EkD0W38ti64"
+BOT_TOKEN = "6374172577:AAEJ8SYD8VDFxvmw6sdxDmqmxbhPbO2PgDY"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/Mvhmed"
 GROUP = "https://t.me/Mvhmed"
